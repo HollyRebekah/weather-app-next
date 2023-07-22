9d0926ac55e808de2fe0e221bd916f7e712799f4
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2023-07-22 13:29:17 +0100

    project structure & test cases written

---
+++ __tests__/components/forecast.test.js
@@ -0,0 +1,5 @@
+describe('forecast', () => {
+    it('it renders a tile for each day of the forecast', () => {
+        // expect number of tiles rendered to equal number of days in forecast data
+    })
+})
---
+++ __tests__/components/searchBar.test.js
@@ -0,0 +1,7 @@
+describe('SearchBar', () => {
+    it('calls the api with the search term', () => {
+        // type in search box
+        // click button
+        // expect api to have been called with search term
+    })
+})
